6e77ea9dd7f48f61dfb1c3326b3882698f283ceb
Victor Navarro <victornavarro@Victors-MBP.lan>
2022-01-16 22:25:43 -0800

    completed color change process

--- button.js
+++ button.js
@@ -2,7 +2,7 @@ function changeColor() {
   var blueDiv = document.getElementById("changeable");
   // Use the Mozilla Developer Network resource to find the right way to change
   // the background for this div 
-  /* Your Code Goes Here */
+  blueDiv.setAttribute('class', 'purple half')
 }
 
 function replaceWord() {
@@ -11,17 +11,15 @@ function replaceWord() {
   fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "your-api-key-here",
-      "x-rapidapi-host": "api-host-here",
+      "x-rapidapi-key": "keys",
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
     },
   })
     .then((response) => {
-      console.log(response);
-      response.json().then(() => {
-        /* 
-          Complete this handler function with code that populates an html 
-          element with the random word 
-        */
+      response.json().then((responseBody) => {
+        console.log(responseBody.word);
+        wordable.textContent = responseBody.word
+        
       });
     })
     .catch((err) => {
